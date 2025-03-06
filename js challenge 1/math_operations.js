8bc495e7a47ecaf9cc28b18710e219053c07eb05
ninajanerulida <ninajanerulida@gmail.com>
2025-03-06 10:54:13 +0800

    Initial commit

---
+++ js challenge 1/math_operations.js
@@ -0,0 +1,56 @@
+// //task 1: calculate the total cost of the items
+// let item1= 10;
+// let item2= 111;
+// let item3= 15;
+
+// let totalCost= item1 + item2+ item3;
+
+// console.log("The total cost of the items is: $" + totalCost);
+
+
+// // task 2: calculate the average of three numbers
+// let num1= 7;
+// let num2= 7;
+// let num3= 8;
+
+// let average= (num1 + num2 + num3)/ 3;
+
+// console.log("The average is: "+ average);
+
+
+// // task 3: Odd or Even number
+
+// let number= 7;
+// if (number % 2===0){
+//     console.log( number + " is an even number");
+// } 
+// else {
+//     console.log(number + " is an odd number.");
+// }
+
+
+// // task 4: calculate discounts
+
+// let originalPrice= 200;
+// let discountPercentage= 30;
+
+// let discountAmount= (originalPrice * discountPercentage)/ 200;
+// let discountedPrice= originalPrice - discountAmount;
+
+// console.log("The discounted price is: $" + discountedPrice);
+
+// BunosTask: Reverse Percentage Calculation
+let finalPrice = 170; 
+let discount = 30;
+
+let originalPrice = finalPrice / (1 - discount / 200);
+
+console.log("The original price before the discount was: $" + originalPrice);
+
+
+
+
+
+
+
+
